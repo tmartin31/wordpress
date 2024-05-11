e89ee5a2e748082c9dbd25c68739c168cd47070c
Tim Martin <tmartin3103@gmail.com>
2024-05-11 15:42:07 +1000

    Add detail to block variable names

--- blocks/feature.php
+++ blocks/feature.php
@@ -4,14 +4,14 @@
 // ACF fields
 //-----------------------------------------------------
 
-$background = wp_kses_post( get_field( 'background' ) );
-$tint = wp_kses_post( get_field( 'tint' ) );
+$background_image_url = wp_kses_post( get_field( 'background' ) );
+$tint_rgba = wp_kses_post( get_field( 'tint' ) );
 
 ?>
 		
-<div class="bg-center bg-cover" style="background-image: url('<?php echo $background; ?>');">
+<div class="bg-center bg-cover" style="background-image: url('<?php echo $background_image_url; ?>');">
 	
-	<div class="py-24 lg:py-48" style="background-color: <?php echo $tint; ?>;">
+	<div class="py-24 lg:py-48" style="background-color: <?php echo $tint_rgba; ?>;">
 		
 		<div class="container prose text-center text-white">
 			
